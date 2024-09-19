078ad8fbc2d6892c72aff7999bb9df960c3c232f
AOmwenga <aomwenga@gmail.com>
2024-09-19 14:44:34 -0500

    Simple number game

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+// Create a simple guessing game that pushes users toward the correct answer in some iterative way. The game does not need to be practical or complicated.
+// Use window object methods to gather input from the user and display information to the user.
+// Use DOM manipulation to give a visual indication of the game's progress in some way.
+
+// declared the input
+let guess = prompt(`Pick a number between 1-3`);
+
+// looping inputs
+if (Number(guess)) {
+  // declared input from guess
+  let input = Number;
+  // if they chose intended winning number
+  if (input == 2) {
+    console.log(`You Win!`);
+    // if the number was wrong
+  } else {
+    if (input > 2 || input < 2) console.log(`OOPS`);
+  }
+  // if the number was the wrong type of input
+} else {
+  console.log(`You Lose`);
+}
